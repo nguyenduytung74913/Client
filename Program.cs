8cd039530d16d406f0a58d47fd6251e39e047d2b
nguyenduytung74913 <63642136+nguyenduytung74913@users.noreply.github.com>
2020-05-25 23:08:52 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,69 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System;
 using System.IO;
 using System.Net;
+using System.Text;
 using System.Net.Sockets;
 
-namespace clientt
+public class Client
 {
-    class Program
+
+    private const int BUFFER_SIZE = 1024;
+    private const int PORT_NUMBER = 8080;
+
+    static ASCIIEncoding encoding = new ASCIIEncoding();
+
+    public static void Main()
     {
-        private const int BUFFER_SIZE = 1024;
-        private const int PORT_NUMBER = 9999;
 
-        static ASCIIEncoding encoding = new ASCIIEncoding();
-
-        public static void Main()
+        try
         {
+            TcpClient client = new TcpClient();
 
-            try
+            // 1. connect
+            client.Connect("127.0.0.1", PORT_NUMBER);
+            Stream stream = client.GetStream();
+
+            Console.WriteLine("Đã kết nối với Máy chủ.");
+            
+            while (true)
             {
-                TcpClient client = new TcpClient();
 
-                // 1. connect
-                client.Connect("127.0.0.1", PORT_NUMBER);
-                Stream stream = client.GetStream();
+                
+                var reader = new StreamReader(stream);
+                var writer = new StreamWriter(stream);
+                writer.AutoFlush = true;
 
-                Console.WriteLine("Connected to Server.");
+                Console.WriteLine("Please enter the number (0-10) / dig[domain.com] / curl[domain.com/myfile.htm] : ");
 
-                while (true)
-                {
+                // 2. send
+                string str = Console.ReadLine();
+                writer.WriteLine(str);
+                writer.AutoFlush = true;
 
-
-                    var reader = new StreamReader(stream);
-                    var writer = new StreamWriter(stream);
-                    writer.AutoFlush = true;
-
-                    Console.WriteLine("Please enter the number (0-10) : ");
-
-                    // 2. send
-                    string str = Console.ReadLine();
-                    writer.WriteLine(str);
-                    writer.AutoFlush = true;
-
-                    // 3. receive
-                    str = reader.ReadLine();
-                    Console.WriteLine(str);
-                    if (str.ToUpper() == "BYE")
-                        break;
-                }
-                // 4. close
-                stream.Close();
-                client.Close();
-            }
-
-            catch (Exception ex)
-            {
-                StringBuilder sb = new StringBuilder();
-                sb.Append(";Disconnect At: " + DateTime.Now + ";" + "Reason: Closed By Server\n");
-                File.AppendAllText("D://Access.log", sb.ToString());
-                sb.Clear();
-                Console.WriteLine("Error: " + ex);
-            }
-
-            Console.Read();
+                // 3. receive
+                str = reader.ReadLine();
+                Console.WriteLine(str);
+                if (str.ToUpper() == "BYE")
+                    break;
+            }                                                                          
+            // 4. close
+            stream.Close();
+            client.Close();
         }
+
+        catch (Exception ex)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(";Ngắt kết nối tại: " + DateTime.Now + ";" + "Lý do: Đóng bởi máy chủ\n");
+            File.AppendAllText("D://Access.log", sb.ToString());
+            sb.Clear();
+            Console.WriteLine("Lỗi: " + ex);
+        }
+
+        Console.Read();
     }
 }
